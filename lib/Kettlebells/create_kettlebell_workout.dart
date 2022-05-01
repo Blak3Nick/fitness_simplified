0a9b697f9de0fec125c2afde6b108581e4abcd99
Blake Nicholson <blaken2021@gmail.com>
2022-05-01 19:04:07 -0400

    Adding new methods to create new kettlebell workouts

---
+++ lib/Kettlebells/create_kettlebell_workout.dart
@@ -0,0 +1,24 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class CreateKettleBellWorkout extends StatelessWidget {
+  const CreateKettleBellWorkout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Workout'),
+      ),
+      body: Column(
+        children: [
+          Row(
+
+          ),
+        ],
+      ),
+    );
+  }
+}
