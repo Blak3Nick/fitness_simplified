6ce802f0b0152342d0bab46aa7ed850eb901760f
Blake Nicholson <blaken2021@gmail.com>
2022-09-02 07:13:41 -0400

    added new workout methods

---
+++ lib/CustomWorkout/workout_home.dart
@@ -0,0 +1,68 @@
+import 'package:fitness_simplified/Kettlebells/kbellgroup.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../models.dart';
+import '../services/firestore.dart';
+import '../shared/bottom_nav.dart';
+import '../shared/error.dart';
+import '../shared/loading.dart';
+
+
+class WorkoutHomeScreen extends StatelessWidget {
+  const WorkoutHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<KettleBellWorkout>>(
+      future: FirestoreService().getKettlebellWorkouts(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const LoadingScreen();
+        } else if (snapshot.hasError) {
+          return Center(
+            child: ErrorMessage(message: snapshot.error.toString()),
+          );
+        } else if (snapshot.hasData) {
+          var kettlebellworkouts = snapshot.data!;
+
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.deepPurple,
+              title: const Text('Kettlebell Workouts'),
+              actions: [
+                IconButton(
+                  icon: Icon(
+                    FontAwesomeIcons.circleUser,
+                    color: Colors.pink[200],
+                  ),
+                  onPressed: () => Navigator.pushNamed(context, '/profile'),
+                )
+              ],
+            ),
+            body:  GridView.count(
+              primary: false,
+              padding: const EdgeInsets.all(20.0),
+              crossAxisSpacing: 10.0,
+              crossAxisCount: 2,
+              children: kettlebellworkouts.map<Widget>((kettlebellworkout) => KbellGroup(kettleBellWorkout: kettlebellworkout)).toList(),
+            ),
+
+            floatingActionButton: FloatingActionButton.extended(
+              onPressed: () {
+                Navigator.pushNamed(context, '/userkettlebellworkouts');
+              },
+              label: const Text('Custom User Workouts'),
+              backgroundColor: Colors.pink,
+            ),
+            bottomNavigationBar: const BottomNavBar(),
+
+          );
+        } else {
+          return const Text('No workouts found. Contact Support');
+        }
+      },
+    );
+  }
+
+}
